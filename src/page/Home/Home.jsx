edf7875ba0a1cc6955e992e5c40f3dbb70b0aa09
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-05-30 18:45:26 +0200

    header design terminé

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import Navigation from '../../components/Navigation/Navigation'
+import React from 'react';
+import "./Home.scss";
 
 export default function Home() {
   return (
     <div>
-      <header>
-        <form >
-          <input type="text" />
-          <button>Search</button>
-        </form>
+
+      <header className='header'>
+       
       </header>
+      <div className='content'>
+        
+      </div>
+      <div className='header-search'>
+        <h1>Find Your Book.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem  Perferendis commodi ducimus consectetur ratione tempora iusto optio! Cum, porro cumque!</p>
+        <form className='form'>
+          <input className='input-text' type="text" />
+          <button className='btn-search'><span className="material-symbols-outlined">
+search
+</span></button>
+        </form>
+      </div>
+     
     </div>
   )
 }
