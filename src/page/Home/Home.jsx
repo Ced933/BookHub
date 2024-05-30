4c02e08530ab774daed2f100ce24f0ef483420b4
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-05-30 16:35:56 +0200

    initialisation du projet bookhub

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <form >
+          <input type="text" />
+          <button>Search</button>
+        </form>
+      </header>
+    </div>
+  )
+}
