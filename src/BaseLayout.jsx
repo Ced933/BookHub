40703a60c0977670f9f0d31a4bf2114cc939961c
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-06-05 20:14:19 +0200

    navigation faite mais non responsive

--- src/BaseLayout.jsx
+++ src/BaseLayout.jsx
@@ -4,6 +4,7 @@ import {Outlet} from 'react-router-dom';
 
 export default function BaseLayout() {
   return (
+    // on a mit flex end car lorsqu'on fait un postion:fixed l'élement sort de son moule et les elements restant se collent à gauche 
     < div style={{ display:'flex', justifyContent:'flex-end'}}>
     <Navigation/>
     <Outlet/>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,17 +7,27 @@ export default function Navigation() {
     <nav className='nav'>
       <div className='nav-box'>
      
-      <div className='div-brand'>
-        {/* <img src="./logo.png" className='img-logo' alt="" /> */}
-        <Link to={'/'} className='title-logo'>MovieHub</Link>
-    
-      </div>
-      {/* <div>
-        <ul className='ul-nav'>
-         <li><NavLink to={"/"} className="link-nav">Home</NavLink></li> 
-         <li><NavLink to={"/about"} className="link-nav">About</NavLink></li> 
-        </ul>
-      </div> */}
+        <div className='div-brand'>
+          <Link to={'/'} className='title-logo'><h3>MovieHub</h3> </Link>
+      
+        </div>
+        <div>
+        <h3 className='movie-title-nav'>CATEGORIES</h3>
+          <ul className='ul-nav'>
+          <li><NavLink to={"/"} className="link-nav">Now playing <span className="material-symbols-outlined">
+live_tv
+</span></NavLink></li> 
+          <li><NavLink to={"/about"} className="link-nav">Popular <span className="material-symbols-outlined">
+thumb_up
+</span></NavLink></li>
+          <li><NavLink to={"/about"} className="link-nav">Top Rated <span className="material-symbols-outlined">
+trending_up
+</span></NavLink></li>
+          <li><NavLink to={"/about"} className="link-nav">Upcoming <span className="material-symbols-outlined">
+event_upcoming
+</span></NavLink></li>
+          </ul>
+        </div>
 
       </div>
     </nav>
